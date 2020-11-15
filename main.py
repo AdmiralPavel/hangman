6727eb76fe4e80364f89ede32d185ffccaa021ef
AdmiralPavel <admiralpavel99@gmail.com>
2020-11-15 17:55:15 +0300

    Added input check

--- main.py
+++ main.py
@@ -27,17 +27,35 @@ def get_guessed_word(word, letters):
     return answer
 
 
+# Функция проверяет корректность ввода пользователя
+def check_input():
+    while True:
+        letter = input().lower()
+        if len(letter) != 1:
+            print('Ошибка! Длина введённой строки не равна 1.')
+        elif letter not in letters:
+            print(f"Ошибка! Введите букву из предложенного списка: {' '.join(letters)}")
+        else:
+            break
+    return letter
+
+
 letters = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
 word = get_random_word()
+lifes = 7
 print('Компьютер загадал слово, попробуйте отгадать его')
-while '_' in get_guessed_word(word, letters):
-    print("Введите букву из предложенного списка: " + ' '.join(letters))
-    letter = input().lower()
+while '_' in get_guessed_word(word, letters) and lifes != 0:
+    print(f"Введите букву из предложенного списка: {' '.join(letters)}")
+    letter = check_input()
     if letter in word:
         print(f'Вы угадали букву {letter}')
     else:
-        print(f'Введённая буква была неверной.')
+        print(f'Введённая буква была неверной. Количество ваших жизней: {lifes}')
+        lifes -= 1
     letters.remove(letter)
     print(f'Отгаданные вами буквы: {get_guessed_word(word, letters)}')
 
-print('Поздравляем, вы выиграли!')
+if lifes == 0:
+    print(f'Вы проиграли! Загаданным словом было {word}')
+else:
+    print('Поздравляем, вы выиграли!')
